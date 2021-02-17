6f500a9639c19c14c3efe4424163bf48d78dc8d4
ParikshitC31 <79221548+ParikshitC31@users.noreply.github.com>
2021-02-18 00:11:24 +0530

    Create multiplication of two numbers.cpp

---
+++ multiplication of two numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x,y;
+    cout<<" enter first no.";
+    cin>>x;
+    cout<<" enter second no";
+    cin>>y;
+    cout<<"product of two numbers="<<x*y;
+ return 0;
+}
+
+
